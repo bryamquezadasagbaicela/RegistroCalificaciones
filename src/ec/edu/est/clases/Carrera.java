93beba1afc7e5bfc09d0184453939df2538aa711
QuezadaBryam bquezadas1@est.ups.edu.ec <>
2019-04-04 19:29:05 -0500

    ejemplo02 programacion orientada a objetos

--- src/ec/edu/est/clases/Carrera.java
+++ src/ec/edu/est/clases/Carrera.java
@@ -5,10 +5,68 @@
  */
 package ec.edu.est.clases;
 
+import java.util.List;
+
 /**
  *
  * @author 59398
  */
 public class Carrera {
+    private int codigo;
+    private String nombre;
+    private List<Materia> materias;
+    private int numeroSemestres;
+    private int numeroEstudiantes;
+    private String titulo;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Materia> getMaterias() {
+        return materias;
+    }
+
+    public void setMaterias(List<Materia> materias) {
+        this.materias = materias;
+    }
+
+    public int getNumeroSemestres() {
+        return numeroSemestres;
+    }
+
+    public void setNumeroSemestres(int numeroSemestres) {
+        this.numeroSemestres = numeroSemestres;
+    }
+
+    public int getNumeroEstudiantes() {
+        return numeroEstudiantes;
+    }
+
+    public void setNumeroEstudiantes(int numeroEstudiantes) {
+        this.numeroEstudiantes = numeroEstudiantes;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
     
+    
+
 }
